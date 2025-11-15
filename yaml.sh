5fcb34c2ded43c9ae7b9f3ad3780f364551ffb68
Rick Lin <rick.cy.lin@gmail.com>
2025-11-15 18:54:02 -0500

    Append project_benches yaml file and update yaml.sh

--- yaml.sh
+++ yaml.sh
@@ -1,3 +1,4 @@
 python3.9 $UVMF_HOME/scripts/yaml2uvmf.py --dest_dir . \
   $(find verification_ip/interface_packages/ -type f -name "*.yaml") \
-  $(find verification_ip/environment_packages/ -type f -name "*.yaml")
+  $(find verification_ip/environment_packages/ -type f -name "*.yaml") \
+  $(find project_benches/lc3/yaml/ -type f -name "*.yaml")
